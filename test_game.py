0b7f8ac7590b05f1cb69170c816679a39e3e8edb
narodr <narodriguez@gradiant.org>
2025-07-01 11:10:42 +0200

    refactor: tests

--- test_game.py
+++ test_game.py
@@ -1,26 +1,16 @@
 from game import Game
 
-def test_score():
-    game = Game()
-    assert isinstance(game.score(), int)
 
-def test_score_all_zero():
-    rolls = [0] * 20
+def test_empty_game():
     game = Game()
-    for pins in rolls:
-        game.roll(pins)
     assert game.score() == 0
 
 def test_score_one_point():
     rolls = [0] * 19 + [1]
-    game = Game()
-    for pins in rolls:
-        game.roll(pins)
+    game = Game(rolls)
     assert game.score() == 1
 
 def test_strike_bonus():
     rolls = [10, 1, 1]
-    game = Game()
-    for pins in rolls:
-        game.roll(pins)
+    game = Game(rolls)
     assert game.score() == 14
